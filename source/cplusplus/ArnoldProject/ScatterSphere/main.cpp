530082f15e2b344d5ecc792ad2aa65f44319f588
hua <8724378+huapoka@user.noreply.gitee.com>
2022-08-19 08:21:28 +0800

    1155

--- source/cplusplus/ArnoldProject/ScatterSphere/main.cpp
+++ source/cplusplus/ArnoldProject/ScatterSphere/main.cpp
@@ -4,9 +4,56 @@
 AI_OPERATOR_NODE_EXPORT_METHODS(OpMethods);
 
 
+void SetShapeArrayFromVector(const std::vector<AtNode*>& n_v, AtNode* node, const char* name)
+{
+    if (n_v.size() < 1) return;
+    AtArray* n_a = AiArrayAllocate(n_v.size(), 1, AI_TYPE_NODE);
+    for (size_t i = 0; i < n_v.size(); i++)
+    {
+        AiArraySetPtr(n_a, i, n_v[i]);
+    }
+    //AiMsgError("size-->%d", AiArrayGetNumElements(n_a));
+    /*AtArray* n_orign = AiNodeGetArray(node, name);
+    AiArrayResize(n_orign, n_v.size()+1, 1);*/
+
+    AiNodeSetArray(node, name, n_a);
+    //AiArrayDestroy(n_a);
+}
+
+void GetShapeArrayToVector(std::vector<AtNode*>& n_v, AtNode* node, const char* name)
+{
+    const AtArray* n_a = AiNodeGetArray(node, name);
+    if (AiArrayGetNumElements(n_a) < 1) return;
+    for (size_t i = 0; i < AiArrayGetNumElements(n_a); i++)
+    {
+        n_v.push_back(static_cast<AtNode*>(AiArrayGetPtr(n_a, i)));
+    }
+}
+
+bool inVector(AtNode* node, const std::vector<AtNode*>& n_v1)
+{
+    for (size_t i = 0; i < n_v1.size(); i++)
+    {
+        if (node == n_v1[i])
+        {
+            return true;
+        }
+    }
+    return false;
+}
+std::vector<AtNode*> SubVector(const std::vector<AtNode*>& n_v, const std::vector<AtNode*>& n_v1)
+{
+    std::vector<AtNode*> out_v;
+    for (size_t i = 0; i < n_v.size(); i++)
+    {
+        if (!inVector(n_v[i], n_v1)) out_v.push_back(n_v[i]);
+    }
+    return out_v;
+}
+
 node_parameters
 {
-    AiParameterStr("selection", "");
+    AiParameterStr("selection_shape", "/*");
 }
 
 operator_init
@@ -21,32 +68,30 @@ operator_cleanup
 
 operator_cook
 {
-
-
-    //循环每一个shape
-    /*AtNodeIterator* shapesIt = AiUniverseGetNodeIterator(AI_NODE_SHAPE);
-    while (!AiNodeIteratorFinished(shapesIt))
-    {
-        AtNode* node_s = AiNodeIteratorGetNext(shapesIt);
-
-
-
-    }
-    AiNodeIteratorDestroy(shapesIt);*/
-
-    const AtNodeEntry* a = AiNodeGetNodeEntry(node);
-    if (AiNodeEntryGetType(a) != AI_NODE_SHAPE) return 1;
-
-    AiMsgWarning("info----->%s", AiNodeGetName(node));
-
-
-
-
     return true;
 }
 
 operator_post_cook
 {
+
+
+    AtString shape_name = AiNodeGetStr(op,"selection_shape");
+    //循环每一个shape
+    AtNodeIterator* shapesIt = AiUniverseGetNodeIterator(AiNodeGetUniverse(op),AI_NODE_SHAPE);//AiUniverseGetNodeIterator(AiNodeGetUniverse(op),AI_NODE_SHAPE);
+    while (!AiNodeIteratorFinished(shapesIt))
+    {
+        AtNode* node_s = AiNodeIteratorGetNext(shapesIt);
+
+        if (AiOpMatchNodeSelection(node_s, shape_name, true, op))
+        {
+            AiNodeSetBool(node_s, "matte", true);
+        }
+        AtNode* parent_node = AiNodeGetParent(node_s);
+        AiMsgWarning("lights_sel-->%s-->%s---->%d----->parent: %d",shape_name.c_str(), AiNodeGetName(node_s), AiNodeGetBool(node_s, "matte"), parent_node==NULL);
+        
+    }
+    AiNodeIteratorDestroy(shapesIt);
+
     return true;
 }
 
--- source/cplusplus/ArnoldProject/Test/main.cpp
+++ source/cplusplus/ArnoldProject/Test/main.cpp
@@ -3,22 +3,41 @@
 #include <cstdio>
 #include <vector>
 
-void SetShapeArrayFromVector(const std::vector<AtNode*>& n_v,AtNode* node,const char* name)
+
+void Render(AtNode* options)
 {
-	AtArray* n_a = AiArrayAllocate(n_v.size(), 1, AI_TYPE_NODE);
-	for (size_t i = 0; i < n_v.size(); i++)
-	{
-		AiArraySetPtr(n_a, i, n_v[i]);
-	}
-	AiNodeSetArray(node, name, n_a);
-}
-void GetShapeArrayToVector(std::vector<AtNode*>& n_v,AtNode* node, const char* name)
-{
-	const AtArray* n_a = AiNodeGetArray(node, name);
-	for (size_t i = 0; i < AiArrayGetNumElements(n_a); i++)
-	{
-		n_v.push_back(static_cast<AtNode*>(AiArrayGetPtr(n_a, i)));
-	}
+	// create a perspective camera
+	AtNode* camera = AiNode("persp_camera");
+	AiNodeSetStr(camera, "name", "mycamera");
+	// position the camera (alternatively you can set 'matrix')
+	AiNodeSetVec(camera, "position", 0.f, 10.f, 35.f);
+	AiNodeSetVec(camera, "look_at", 0.f, 3.f, 0.f);
+	AiNodeSetFlt(camera, "fov", 45.f);
+
+	// get the global options node and set some options
+	AiNodeSetInt(options, "AA_samples", 8);
+	AiNodeSetInt(options, "xres", 480);
+	AiNodeSetInt(options, "yres", 360);
+	AiNodeSetInt(options, "GI_diffuse_depth", 4);
+	AiNodeSetPtr(options, "camera", camera);
+
+	// create an output driver node
+	AtNode* driver = AiNode("driver_jpeg");
+	AiNodeSetStr(driver, "name", "mydriver");
+	AiNodeSetStr(driver, "filename", "scene1.jpg");
+
+	// create a gaussian filter node
+	AtNode* filter = AiNode("gaussian_filter");
+	AiNodeSetStr(filter, "name", "myfilter");
+
+	// assign the driver and filter to the main (beauty) AOV,
+	// which is called "RGBA" and is of type RGBA
+	AtArray* outputs_array = AiArrayAllocate(1, 1, AI_TYPE_STRING);
+	AiArraySetStr(outputs_array, 0, "RGBA RGBA myfilter mydriver");
+	AiNodeSetArray(options, "outputs", outputs_array);
+
+	// finally, render the image!
+	AiRender(AI_RENDER_MODE_CAMERA);
 }
 int main()
 {
@@ -27,42 +46,41 @@ int main()
 	AiMsgSetLogFileName("scene1.log");
 	AiMsgSetConsoleFlags(AI_LOG_ALL);
 
-	
-
-
-	//AtNode* create_sphere = AiNode("create_spheres");
-	/*AtArray* a = AiArray(6, 2, AI_TYPE_VECTOR2);
-	uint32_t num_a = AiArrayGetNumElements(a);
-	uint8_t num_b = AiArrayGetNumKeys(a);
-	size_t num_c = AiArrayGetDataSize(a);
-	size_t num_d = AiArrayGetKeySize(a);
-
-
-	char str[80];
-	sprintf(str, "%d-->%d-->%d-->%d-->%d", num_a,num_b,num_c,num_d,sizeof(float));
-	AiMsgDebug(str);*/
+	AiASSLoad("E:/Work/test/maya/a.ass");
 
 	AtNode* sphere = AiNode("sphere", "sphere0");
-	std::vector<AtNode*> a;
 	AtNode* light0=AiNode("point_light", "light0");
 	AtNode* light1=AiNode("point_light", "light1");
-	a.push_back(light0);
-	a.push_back(light1);
 
-	//AiNodeSetArray(sphere, "light_group", n_p);
-	SetShapeArrayFromVector(a, sphere, "light_group");
 
-	//AiASSWrite("test.ass");
-	std::vector<AtNode*> b;
-	GetShapeArrayToVector(b, sphere, "light_group");
 
-	for (AtNode* a : b)
+	AtNode* u = AiNodeLookUpByName("/Chars_xiaogui_1/Chars_xiaogui_1Shape");
+	/*if (u)
 	{
-		AiMsgDebug(AiNodeGetName(a));
-	}
+		void* u_data=AiNodeGetPluginData(u);
+
+
+		AiMsgDebug("--->%s-->", AiNodeGetName(u), procedural_num_nodes(u,u_data));
+	}*/
+	/*AiNodeGetPluginData()
+	AtProcNumNodes();
+	AtProcGetNode();*/
 
 
 
+
+
+
+
+	AtNode* make_info = AiNode("make_info", "makeinfos");
+	AtNode* options = AiUniverseGetOptions();
+	AiNodeSetPtr(options, "operator", static_cast<void*>(make_info));
+
+
+	
+
+	//AiMsgDebug("--->%d",AiNodeIs(make_info,AtString("make_info")));
+	Render(options);
 	AiEnd();
 
 	return 0;
